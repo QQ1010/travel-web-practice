30f2a58dec88edfdd112f3162586f124ed081e55
QQ1010 <asdfg335522@gmail.com>
2021-02-10 16:32:21 +0800

    2021/02/10

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask,url_for,render_template,request,redirect,flash
 
 app = Flask(__name__)
 
-@app.route('/login', methods=['GET', 'POST']) 
+@app.route('/', methods=['GET', 'POST']) 
 def login():
     if request.method == 'POST': 
         if login_check(request.form['username'],request.form['password']):
@@ -15,11 +15,19 @@ def login_check(username,password):
     else:
         return False
 
-@app.route('/menu/<username>')
+@app.route('/menu/<username>',methods=['GET', 'POST'])
 def menu(username):
     return render_template('menu.html',username=username)
 
-
+@app.route('/prepare',methods=['GET', 'POST'])
+def prepare():
+    return render_template('prepare.html')
+@app.route('/implement' ,methods=['GET','POST'])
+def implement():
+    return render_template('implement.html')
+@app.route('/review' ,methods=['GET','POST'])
+def review():
+    return render_template('review.html')
 if __name__ == '__main__':
     app.debug = True
     app.secret_key = '123456'
